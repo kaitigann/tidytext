cbe3d61839b9e915cc5aa228fc582aede6c57a51
David Kim <davidskim89@gmail.com>
2018-12-22 18:29:09 -0800

    Fixed bug where multiple words in the same chunk wouldn't be replaced.  Added feature to randomize replacement word. Added linting (Airbnb)

--- content.js
+++ content.js
@@ -1,108 +1,51 @@
-var elements = document.getElementsByTagName('*');
+const elements = document.getElementsByTagName('*');
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+const fReplacementWords = ['frack', 'frick', 'flip', 'fudge', 'flim-flam', 'fraggle'];
+const sReplacementWords = ['short', 'snickerdoodle', 'spit', 'sugar', 'poop', 'shoot'];
+const dReplacementWords = ['dang', 'darn', 'daggum', 'doggon', 'jam', 'ham', 'spam'];
+const aReplacementWords = ['booty', 'ask', 'bum', 'fanny'];
+const bReplacementWords = ['birch', 'witch', 'bitcoin', 'beast'];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+for (let i = 0; i < elements.length; i += 1) {
+  const element = elements[i];
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;	
-			
-			if (text.match(/fuck/gi) != null){
-				replaceFWord(text, node);
-		    }
-			if (text.match(/shit/gi) != null){
-				replaceSWord(text, node);
-		    }
-			
-			if (text.match(/damn/gi) != null){
-				replaceDWord(text, node);
-		    }
-			
-			if (text.match(/ass/gi) != null){
-				replaceAWord(text, node);
-		    }
-			if (text.match(/bitch/gi) != null){
-				replaceBWord(text, node);
-		    }
-        }
+  for (let j = 0; j < element.childNodes.length; j += 1) {
+    const node = element.childNodes[j];
+
+    if (node.nodeType === 3) {
+      const text = node.nodeValue;
+      let replacementText = text;
+
+      while (
+        replacementText.toLowerCase().includes('ass')
+        || replacementText.toLowerCase().includes('shit')
+        || replacementText.toLowerCase().includes('damn')
+        || replacementText.toLowerCase().includes('fuck')
+        || replacementText.toLowerCase().includes('bitch')
+      ) {
+        // replacements
+        replacementText = replacementText.replace(
+          /ass/i,
+          aReplacementWords[Math.floor(Math.random() * aReplacementWords.length)],
+        );
+        replacementText = replacementText.replace(
+          /shit/i,
+          sReplacementWords[Math.floor(Math.random() * sReplacementWords.length)],
+        );
+        replacementText = replacementText.replace(
+          /damn/i,
+          dReplacementWords[Math.floor(Math.random() * dReplacementWords.length)],
+        );
+        replacementText = replacementText.replace(
+          /fuck/i,
+          fReplacementWords[Math.floor(Math.random() * fReplacementWords.length)],
+        );
+        replacementText = replacementText.replace(
+          /bitch/i,
+          bReplacementWords[Math.floor(Math.random() * bReplacementWords.length)],
+        );
+      }
+      node.replaceWith(document.createTextNode(replacementText));
     }
+  }
 }
-
-function replaceFWord(text, node) {
-	var replacementWords = ["frack", "frick", "flip", "fudge", "flim-flam", "fraggle"];
-	var replacedText = text.replace(/fuck/gi, replacementWords[Math.floor(Math.random() * 5)]);
-
-     if (replacedText !== text) {
-         node.replaceWith(document.createTextNode(replacedText));
-      }
-}
-
-function replaceSWord(text, node) {
-	var replacementWords = ["short", "snickerdoodle", "spit", "sugar", "poop", "shoot"];
-	var replacedText = text.replace(/shit/gi, replacementWords[Math.floor(Math.random() * 5)]);
-
-     if (replacedText !== text) {
-         node.replaceWith(document.createTextNode(replacedText));
-      }
-}
-
-function replaceDWord(text, node) {
-	
-	var replacementWords = ["dang", "darn", "daggum", "doggon", "jam", "ham", "spam"];
-
-	var replacedText = text.replace(/damn/gi, replacementWords[Math.floor(Math.random() * 6)]);
-     if (replacedText !== text) {
-         node.replaceWith(document.createTextNode(replacedText));
-      }
-}
-
-function replaceAWord(text, node) {
-	
-	var replacementWords = ["booty", "ask", "bum", "fanny"];
-
-	var replacedText = text.replace(/ass/gi, replacementWords[Math.floor(Math.random() * 3)]);
-     if (replacedText !== text) {
-         node.replaceWith(document.createTextNode(replacedText));
-      }
-}
-
-function replaceBWord(text, node) {
-	
-	var replacementWords = ["birch", "witch", "bitcoin", "beast"];
-
-	var replacedText = text.replace(/bitch/gi, replacementWords[Math.floor(Math.random() * 3)]);
-     if (replacedText !== text) {
-         node.replaceWith(document.createTextNode(replacedText));
-      }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
